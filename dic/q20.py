27b8c5da82a18fc63f28f7dff773763ffe0e10b0
prateek-rtk <65495328+prateek-rtk@users.noreply.github.com>
2020-10-01 10:30:25 +0530

    Update q20.py

--- dic/q20.py
+++ dic/q20.py
@@ -1,7 +1,7 @@
 # Write a Python program to count the values associated with key in a dictionary. Go to the editor
 Sampledata = [{'id': 1, 'success': True, 'name': 'Lary'}, {
     'id': 2, 'success': False, 'name': 'Rabi'}, {'id': 3, 'success': True, 'name': 'Alex'}]
-count = 0
+count =int(input("id?"))
 for i in Sampledata:
     if 'success' in i.keys():
         if i['success'] == True:
