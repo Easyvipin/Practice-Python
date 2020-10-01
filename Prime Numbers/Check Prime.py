424ec9670f4cb07c00732b20067d1da19a6250a9
Hemang Vyas <vyashemang563@gmail.com>
2020-10-01 22:30:17 +0530

    Update Check Prime.py
    I tried to make the code look cleaner and easily understandable.

--- Prime Numbers/Check Prime.py
+++ Prime Numbers/Check Prime.py
@@ -2,16 +2,23 @@
 # coding: utf-8
 
 # In[ ]:
+    
+    
+'''
+check_prime returns true if the given number is prime else it returns false.
+'''
+def check_prime(number):
+    if(number <= 1):
+        return False
+    
+    for i in range(2, number//2):
+        if(number % i) == 0:
+            return False
+    return True
 
+number  = int(input("Enter the number: "))
 
-num = int(input())
-if num > 1: 
-    for i in range(2, num//2):  
-        if (num % i) == 0: 
-            print("Not Prime") 
-            break
-    else: 
-        print("Prime") 
-else: 
-    print(num, "Not Prime") 
-
+if(check_prime(number)):
+    print("Number is Prime")
+else:
+    print("Number is not Prime")
